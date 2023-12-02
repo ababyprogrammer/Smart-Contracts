30d2466c58416903133d7aa04faf733dfa58fa6b
A baby programmer <139476654+ababyprogrammer@users.noreply.github.com>
2023-12-02 11:14:24 +0330

    Add files via upload

---
+++ Coin/coin_1.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.0;
+
+contract Coin {
+    string public name;
+    string public symbol;
+    uint8 public decimals;
+    uint256 public totalSupply;
+
+    mapping(address => uint256) public balanceOf;
+
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, uint256 _totalSupply) public {
+        name = _name;
+        symbol = _symbol;
+        decimals = _decimals;
+        totalSupply = _totalSupply;
+        balanceOf[msg.sender]
+    }
+}
